79c1b753ea3995d59d63c31770082c9fcd5adadd
hoanglong57hn <hoanglong57hn@gmail.com>
2024-07-17 04:30:26 +0700

    Add

--- src/java/dal/DBContext.java
+++ src/java/dal/DBContext.java
@@ -13,7 +13,7 @@ public class DBContext {
     public DBContext() {
         try {
             String user = "sa";
-            String pass = "123";
+            String pass = "123456";
             String url = "jdbc:sqlserver://localhost\\SQLEXPRESS:1433;databaseName=Assgn_PRJ301";
             Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
             connection = DriverManager.getConnection(url, user, pass);
